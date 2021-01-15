8718f9ad016717a7ca87c5df5796b4620c172fe7
maestre3d <luis.alonso.16@hotmail.com>
2021-01-15 01:51:05 -0700

    Remove notifier unused args

--- internal/service/book_notifier.go
+++ internal/service/book_notifier.go
@@ -9,5 +9,5 @@ import (
 // BookNotifier publishes or alerts somebody and/or something (e.g. admin, external system)
 type BookNotifier interface {
 	// Notify push notification when an aggregate.Book state was mutated
-	Notify(ctx context.Context, b aggregate.Book) error
+	Notify(context.Context, aggregate.Book) error
 }
