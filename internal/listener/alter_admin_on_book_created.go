f08889628d5615649812d59c677cbf809b05f936
maestre3d <luis.alonso.16@hotmail.com>
2021-01-15 01:52:32 -0700

    Add initial event listeners

---
+++ internal/listener/alter_admin_on_book_created.go
@@ -0,0 +1,5 @@
+package listener
+
+// AlertAdminOnBookCreated notifies admin through either email or push notification when an aggregate.Book was created
+type AlertAdminOnBookCreated struct {
+}
---
+++ internal/listener/listener.go
@@ -0,0 +1,15 @@
+package listener
+
+import (
+	"context"
+
+	"github.com/maestre3d/newton/internal/event"
+)
+
+// Listener subscribes to a domain event in order to execute tasks asynchronously and independently
+type Listener interface {
+	// SubscribedTo returns the event.DomainEvent this specific listener is subscribed to
+	SubscribedTo() event.DomainEvent
+	// On executes the required tasks while receiving the subscribed event.DomainEvent as argument
+	On(context.Context, event.DomainEvent) error
+}
