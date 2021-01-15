f4f3cdc59a4a64a5f9c193f62ad795ede2498687
maestre3d <luis.alonso.16@hotmail.com>
2021-01-15 01:07:33 -0700

    Add notifier comments

--- internal/service/book_notifier.go
+++ internal/service/book_notifier.go
@@ -8,5 +8,6 @@ import (
 
 // BookNotifier publishes or alerts somebody and/or something (e.g. admin, external system)
 type BookNotifier interface {
+	// Notify push notification when an aggregate.Book state was mutated
 	Notify(ctx context.Context, b aggregate.Book) error
 }
