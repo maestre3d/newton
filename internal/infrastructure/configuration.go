cdb4b9b2980f3e889117f07440e184c498da225f
maestre3d <luis.alonso.16@hotmail.com>
2021-01-14 23:45:29 -0700

    Add book notifier

--- internal/infrastructure/configuration.go
+++ internal/infrastructure/configuration.go
@@ -9,22 +9,15 @@ import (
 type Configuration struct {
 	Stage       string
 	Version     string
+	AdminEmail  string
 	DynamoTable string
 }
 
 func init() {
 	viper.SetDefault("newton.stage", DevStage)
 	viper.SetDefault("newton.version", "1.0.0")
+	viper.SetDefault("newton.admin_email", "luis.alonso.16@hotmail.com")
 	viper.SetDefault("newton.dynamo.table", "newton-books-dev")
-}
-
-// NewConfiguration creates a Configuration with default configs or from sources
-func NewConfiguration() Configuration {
-	return Configuration{
-		Stage:       viper.GetString("newton.stage"),
-		Version:     viper.GetString("newton.version"),
-		DynamoTable: viper.GetString("newton.dynamo.table"),
-	}
 }
 
 const (
@@ -33,6 +26,16 @@ const (
 	// DevStage Development deployment stage
 	DevStage = "dev"
 )
+
+// NewConfiguration creates a Configuration with default configs or from sources
+func NewConfiguration() Configuration {
+	return Configuration{
+		Stage:       viper.GetString("newton.stage"),
+		Version:     viper.GetString("newton.version"),
+		AdminEmail:  viper.GetString("newton.admin_email"),
+		DynamoTable: viper.GetString("newton.dynamo.table"),
+	}
+}
 
 // IsProd returns if current config stage is in production stage
 func (c Configuration) IsProd() bool {
