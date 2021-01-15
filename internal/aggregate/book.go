1173592a9864495ca3b67b30ec4c9e37cacd6e74
maestre3d <luis.alonso.16@hotmail.com>
2021-01-14 22:00:10 -0700

    Add book aggregate values

--- internal/aggregate/book.go
+++ internal/aggregate/book.go
@@ -1,8 +1,27 @@
 package aggregate
 
-import "github.com/maestre3d/newton/internal/valueobject"
+import (
+	"time"
+
+	"github.com/maestre3d/newton/internal/valueobject"
+)
 
 // Book eBook or document uploaded by users
 type Book struct {
 	ID valueobject.BookID
+	// Book's display name
+	Title string
+	// User's username who uploaded this book, since usernames are unique and immutable, high-cardinality is ensured
+	Uploader string
+	// Potential data replication/projection. Use IN statement query to lookup inside this field
+	Authors     []string
+	PublishYear int
+	// Potential data replication/projection
+	Categories []string
+	// S3 url Book's cover image reference
+	Cover string
+	// S3 url media reference
+	ExternalLink string
+	CreateTime   time.Time
+	UpdateTime   time.Time
 }
