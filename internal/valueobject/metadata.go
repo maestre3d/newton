c4f89358d11c91b5f671123c7ca2073577d0cd00
maestre3d <luis.alonso.16@hotmail.com>
2021-01-15 21:45:41 -0700

    Add metadata value object

---
+++ internal/valueobject/metadata.go
@@ -0,0 +1,14 @@
+package valueobject
+
+import "time"
+
+// Metadata default Newton metadata of an aggregate; lets developers soft-remove, restore and hard-remove an specific
+// aggregate
+//	This value object is using enriched built-in data types, apart from being a helper object.
+//	Hence, validation and encapsulation is not required.
+type Metadata struct {
+	CreateTime    time.Time
+	UpdateTime    time.Time
+	State         bool
+	MarkAsRemoval bool
+}
