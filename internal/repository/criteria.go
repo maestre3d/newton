b4f3447bdf27aba9afd312b09a7c522e1c50827d
maestre3d <luis.alonso.16@hotmail.com>
2021-01-17 20:00:39 -0700

    Add criteria lite factory

--- internal/repository/criteria.go
+++ internal/repository/criteria.go
@@ -2,6 +2,17 @@ package repository
 
 // Criteria Newton DSL query lang
 type Criteria struct {
-	Limit    int
-	NextPage string
+	Limit    int    `json:"limit"`
+	NextPage string `json:"next_page"`
+}
+
+// NewCriteria creates and ensures criteria
+func NewCriteria(l int, nextPage string) *Criteria {
+	if l <= 0 {
+		l = 100
+	}
+	return &Criteria{
+		Limit:    l,
+		NextPage: nextPage,
+	}
 }
