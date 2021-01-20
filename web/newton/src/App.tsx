cd442bccdc906f3d54b3e343ad6fa0d8a092942d
maestre3d <luis.alonso.16@hotmail.com>
2021-01-20 14:36:21 -0700

    Move styletron and base provier to App component

--- web/newton/src/App.tsx
+++ web/newton/src/App.tsx
@@ -1,14 +1,24 @@
 import './App.css'
 import { BrowserRouter as Router } from 'react-router-dom'
 import DefaultShell from './app/shared/component/default-shell/Default-Shell'
+import { BaseProvider, LightTheme } from 'baseui'
+import { Provider as StyletronProvider, DebugEngine } from 'styletron-react'
+import { Client as Styletron } from 'styletron-engine-atomic'
+
+const debug = process.env.NODE_ENV === 'production' ? void 0 : new DebugEngine()
+const engine = new Styletron()
 
 // App root component
 function App() {
   return (
-    <Router>
-      <DefaultShell />
-    </Router>
-  );
+    <StyletronProvider value={engine} debug={debug} debugAfterHydration>
+      <BaseProvider theme={LightTheme}>
+        <Router>
+          <DefaultShell />
+        </Router>
+      </BaseProvider>
+    </StyletronProvider>
+  )
 }
 
 export default App
--- web/newton/src/index.tsx
+++ web/newton/src/index.tsx
@@ -3,20 +3,10 @@ import ReactDOM from 'react-dom'
 import './index.scss'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
-import { BaseProvider, DarkTheme } from 'baseui'
-import { Provider as StyletronProvider, DebugEngine } from 'styletron-react'
-import { Client as Styletron } from 'styletron-engine-atomic'
-
-const debug = process.env.NODE_ENV === 'production' ? void 0 : new DebugEngine()
-const engine = new Styletron()
 
 ReactDOM.render(
   <React.StrictMode>
-    <StyletronProvider value={engine} debug={debug} debugAfterHydration>
-      <BaseProvider theme={DarkTheme}>
-        <App />
-      </BaseProvider>
-    </StyletronProvider>
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 )
