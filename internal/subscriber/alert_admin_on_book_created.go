85187b221185c88361de098f2f977ec3d5554b7c
A Ruiz <luis.alonso.16@hotmail.com>
2021-01-22 23:06:06 -0700

    Add update author on author image upload, Refactor alert admin on book created handler to more idiomatic way

--- internal/subscriber/alert_admin_on_book_created.go
+++ internal/subscriber/alert_admin_on_book_created.go
@@ -1,6 +1,14 @@
 package subscriber
 
+import (
+	"context"
+
+	"github.com/maestre3d/newton/internal/application"
+	"github.com/maestre3d/newton/internal/event"
+)
+
 // AlertAdminOnBookCreated notifies admin through either email or push notification when an
 // aggregate.Book was created
-type AlertAdminOnBookCreated struct {
+func AlertAdminOnBookCreated(app *application.Author, ctx context.Context, ev event.BookCreated) error {
+	return nil
 }
---
+++ internal/subscriber/update_author_on_author_image_uploaded.go
@@ -0,0 +1,22 @@
+package subscriber
+
+import (
+	"context"
+
+	"github.com/maestre3d/newton/internal/application"
+	"github.com/maestre3d/newton/internal/event"
+	"github.com/maestre3d/newton/internal/valueobject"
+)
+
+// UpdateAuthorOnAuthorImageUploaded projects the new Author's image url into main persistence store
+func UpdateAuthorOnAuthorImageUploaded(app *application.Author, ctx context.Context, ev event.AuthorImageUploaded) error {
+	id, err := valueobject.NewAuthorID(ev.AuthorID)
+	if err != nil {
+		return err
+	}
+	image, err := valueobject.NewImage(ev.Image)
+	if err != nil {
+		return err
+	}
+	return app.Modify(ctx, id, "", "", image)
+}
