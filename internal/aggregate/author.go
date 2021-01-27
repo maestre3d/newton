f7467088b77bff88de18e35df44cb5c0ff45b8fb
A Ruiz <luis.alonso.16@hotmail.com>
2021-01-26 23:31:59 -0700

    Fix unexported event field

--- internal/aggregate/author.go
+++ internal/aggregate/author.go
@@ -17,7 +17,7 @@ type Author struct {
 	Image       valueobject.Image
 
 	Metadata valueobject.Metadata
-	events   []event.DomainEvent
+	Events   []event.DomainEvent
 }
 
 var (
@@ -47,7 +47,7 @@ func NewAuthor(id valueobject.AuthorID, name valueobject.DisplayName, createBy v
 			MarkAsMutation: false,
 			MarkAsRemoval:  false,
 		},
-		events: []event.DomainEvent{
+		Events: []event.DomainEvent{
 			event.AuthorCreated{
 				AuthorID:    id.Value(),
 				DisplayName: name.Value(),
@@ -67,7 +67,7 @@ func (a *Author) Update(name valueobject.DisplayName, createBy valueobject.Usern
 	a.Image = image
 	a.Metadata.UpdateTime = currentTime
 	a.Metadata.MarkAsMutation = true
-	a.events = append(a.events, event.AuthorUpdated{
+	a.Events = append(a.Events, event.AuthorUpdated{
 		AuthorID:    a.ID.Value(),
 		DisplayName: a.DisplayName.Value(),
 		CreateBy:    a.CreateBy.Value(),
@@ -82,7 +82,7 @@ func (a *Author) ChangeState(s bool) {
 	a.Metadata.State = s
 	a.Metadata.UpdateTime = currentTime
 	if s {
-		a.events = append(a.events, event.AuthorRestored{
+		a.Events = append(a.Events, event.AuthorRestored{
 			AuthorID:    a.ID.Value(),
 			DisplayName: a.DisplayName.Value(),
 			RestoreTime: currentTime.Format(time.RFC3339),
@@ -90,7 +90,7 @@ func (a *Author) ChangeState(s bool) {
 		return
 	}
 
-	a.events = append(a.events, event.AuthorDeactivated{
+	a.Events = append(a.Events, event.AuthorDeactivated{
 		AuthorID:    a.ID.Value(),
 		DisplayName: a.DisplayName.Value(),
 		DeleteTime:  currentTime.Format(time.RFC3339),
@@ -100,7 +100,7 @@ func (a *Author) ChangeState(s bool) {
 // Remove marks aggregate as removed
 func (a *Author) Remove() {
 	a.Metadata.MarkAsRemoval = true
-	a.events = append(a.events, event.AuthorRemoved{
+	a.Events = append(a.Events, event.AuthorRemoved{
 		AuthorID:    a.ID.Value(),
 		DisplayName: a.DisplayName.Value(),
 		DeleteTime:  time.Now().UTC().Format(time.RFC3339),
@@ -109,7 +109,7 @@ func (a *Author) Remove() {
 
 // UploadPicture stores and overrides (if required) the current Author image
 func (a *Author) UploadPicture(imageUrl string) {
-	a.events = append(a.events, event.AuthorImageUploaded{
+	a.Events = append(a.Events, event.AuthorImageUploaded{
 		AuthorID: a.ID.Value(),
 		Image:    imageUrl,
 	})
@@ -117,8 +117,7 @@ func (a *Author) UploadPicture(imageUrl string) {
 
 // PullEvents retrieves and flushes all occurred events
 func (a *Author) PullEvents() []event.DomainEvent {
-	memo := make([]event.DomainEvent, len(a.events))
-	copy(a.events, memo)
-	a.events = []event.DomainEvent{}
+	memo := a.Events
+	a.Events = []event.DomainEvent{}
 	return memo
 }
