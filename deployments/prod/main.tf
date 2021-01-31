e5ae79a55dc586b28dd30b57e0f05ca433e7039a
A Ruiz <luis.alonso.16@hotmail.com>
2021-01-31 14:01:42 -0700

    Add terraform aws regions

--- deployments/prod/main.tf
+++ deployments/prod/main.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "~> 3.0"
     }
   }
@@ -9,4 +9,19 @@ terraform {
 
 provider "aws" {
   region = "us-east-1"
+}
+
+provider "aws" {
+  alias  = "us-west"
+  region = "us-west-1"
+}
+
+provider "aws" {
+  alias  = "eu-central"
+  region = "eu-central-1"
+}
+
+provider "aws" {
+  alias  = "ap-east"
+  region = "ap-east-1"
 }
